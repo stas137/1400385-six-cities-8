f8c8a5b95951dccbc1ca84c5482dc555be285e2a
Stas <stasccb@gmail.com>
2021-10-01 18:42:19 +0300

    Начинаем программировать

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const countCard = 5;
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      count={countCard}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
