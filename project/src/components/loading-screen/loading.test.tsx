33e9799d49d06d8eabdc1aef835261d7ed427e35
Stas <stasccb@gmail.com>
2021-11-16 18:12:25 +0300

    Прикрой меня тестом

--- project/src/components/loading-screen/loading.test.tsx
+++ project/src/components/loading-screen/loading.test.tsx
@@ -3,7 +3,6 @@ import LoadingScreen from './loading-screen';
 
 describe('Component: LoadingScreen', () => {
   it('should render correctly', () => {
-
     render(<LoadingScreen />);
     expect(screen.getByText(/Loading.../i)).toBeInTheDocument();
   });
